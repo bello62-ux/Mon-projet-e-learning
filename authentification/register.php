0497631c152b74c1c25898938057aeb0b17b99c4
Adjaho <belloadjaho6213@gmail.com>
2026-03-18 18:49:18 +0100

    chrore:Change the CSS path in the register.php file

--- authentification/register.php
+++ authentification/register.php
@@ -28,7 +28,7 @@ if ($class_result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../asset/css/style.css">
     <title>Inscription - E-learning</title>
 </head>
 <body>
